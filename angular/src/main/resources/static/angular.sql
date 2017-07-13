3b161dec93222202a477735f2ea8cdf2e5a2dc98
siriuszhang <cpurun100@gmail.com>
2017-07-13 16:31:09 +0800

    第7章

--- angular/src/main/resources/static/angular.sql
+++ angular/src/main/resources/static/angular.sql
@@ -1,23 +1,24 @@
 
 
 
-CREATE TABLE IF NOT EXISTS `product` (
-    `name` varchar(64) NOT NULL COMMENT '名字',
-    `description` varchar(64) NOT NULL COMMENT '描述',
-    `category` varchar(64) NOT NULL COMMENT '类型',
-    `price` decimal(10,2) NOT NULL COMMENT '价格',
-    `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '生成时间', 
-    PRIMARY KEY (`name`) ,
-    UNIQUE (`name`)
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL COMMENT 'id',
+  `name` varchar(64) NOT NULL COMMENT '名字',
+  `description` varchar(64) NOT NULL COMMENT '描述',
+  `category` varchar(64) NOT NULL COMMENT '类型',
+  `price` decimal(10,2) NOT NULL COMMENT '价格',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '生成时间',
+  PRIMARY KEY (`id`),
+  UNIQUE(`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='保险类型信息';
 
-INSERT INTO `product`(`name`, `description`, `category`, `price`, `create_time`) VALUES 
-('Kayak', 'A boat for one person', 'Watersports', 275, CURRENT_TIMESTAMP),
-('Lifejacket', 'Protective and fashionable', 'Watersports', 48.95, CURRENT_TIMESTAMP),
-('Soccer Ball', 'FIFA-approved size and weigth', 'Soccer', 19.5, CURRENT_TIMESTAMP),
-('Corner Flags', 'Give your playing field a professional touch', 'Soccer', 34.95, CURRENT_TIMESTAMP),
-('Stadium', 'Flat-packed 35,000-seat stadium', 'Soccer', 79500.00, CURRENT_TIMESTAMP),
-('Thinking Cap', 'Improve your brain efficiency by 75%', 'chess', 16, CURRENT_TIMESTAMP),
-('Unsteady Chair', 'Secretly give your opponent a disadvantage', 'chess', 29.95, CURRENT_TIMESTAMP),
-('Humam Chess Board', 'A fun game for the family', 'chess', 75, CURRENT_TIMESTAMP),
-('Bliing-Bling King', 'Gold-plated, diamond-studded King', 'chess', 75, CURRENT_TIMESTAMP);
+INSERT INTO `product`(`id`, `name`, `description`, `category`, `price`, `create_time`) VALUES 
+(1, 'Kayak', 'A boat for one person', 'Watersports', 275, CURRENT_TIMESTAMP),
+(2, 'Lifejacket', 'Protective and fashionable', 'Watersports', 48.95, CURRENT_TIMESTAMP),
+(3, 'Soccer Ball', 'FIFA-approved size and weigth', 'Soccer', 19.5, CURRENT_TIMESTAMP),
+(4, 'Corner Flags', 'Give your playing field a professional touch', 'Soccer', 34.95, CURRENT_TIMESTAMP),
+(5, 'Stadium', 'Flat-packed 35,000-seat stadium', 'Soccer', 79500.00, CURRENT_TIMESTAMP),
+(6, 'Thinking Cap', 'Improve your brain efficiency by 75%', 'chess', 16, CURRENT_TIMESTAMP),
+(7, 'Unsteady Chair', 'Secretly give your opponent a disadvantage', 'chess', 29.95, CURRENT_TIMESTAMP),
+(8, 'Humam Chess Board', 'A fun game for the family', 'chess', 75, CURRENT_TIMESTAMP),
+(9, 'Bliing-Bling King', 'Gold-plated, diamond-studded King', 'chess', 75, CURRENT_TIMESTAMP);

801b7c1ce81315342bd5fd9cecd0544edd07613b
siriuszhang <cpurun100@gmail.com>
2017-07-13 11:40:03 +0800

    网页资源移动到服务器static目录下

--- angular/src/main/resources/static/sportsStore.js
+++ angular/src/main/resources/static/sportsStore.js
@@ -1,12 +1,12 @@
 angular.module("sportsStore")
-.constant("dataUrl", "http://localhost:8080/angular/Products")
+.constant("dataUrl", "http://47.90.107.224:8080/angular/Products")
 .controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
     
     $scope.data = {};
 
     $http.get(dataUrl)
     .success(function(data) {
-        $scope.data = data;
+        $scope.data.products = data.data;
     })
     .error(function(error){
         $scope.data.error = error;

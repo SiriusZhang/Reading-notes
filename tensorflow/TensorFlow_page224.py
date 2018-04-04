b03d71922c4400a34c02b9d13a3c0b3895ac1327
sirius.zhang <sirius.zhang@weshareholdings.com>
2018-04-04 08:47:07 +0800

    Tensorflow

--- tensorflow/TensorFlow_page224.py
+++ tensorflow/TensorFlow_page224.py
@@ -21,34 +21,43 @@ SAMPLE_GAP = 0.01
 def generate_data(seq):
     x = []
     y = []
+    print(len(seq) - TIMESTEPS -1)
     for i in range(len(seq) - TIMESTEPS -1):
         x.append([seq[i: i + TIMESTEPS]])
         y.append([seq[i + TIMESTEPS]])
     return np.array(x, dtype=np.float32), np.array(y, dtype=np.float32)
 
-def lstm_model(x, y):
-    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(HIDDEN_SIZE)
-    cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * NUM_LAYERS)
-    x_ = tf.unstack(x, axis = 1)
+def lstm_model(X,y):
+    # 创建深度LSTM，深度为 HIDDEN_SIZE
+    lstm_cell = tf.contrib.rnn.BasicLSTMCell(HIDDEN_SIZE, state_is_tuple=True)
+    # 将 lstm_cell 变为多层RNN，层数为NUM_LAYERS
+    cell = tf.contrib.rnn.MultiRNNCell([lstm_cell] * NUM_LAYERS)
+    # 训练rnn，output为输出的结果，_ 返回的是最终的状态
+    output,_ = tf.nn.dynamic_rnn(cell, X,dtype=tf.float32)
+    # 将output 重塑成 n×HIDDEN_SIZE 的矩阵，即每行属于同一层
+    output = tf.reshape(output,[-1, HIDDEN_SIZE])
+    # 创建一个全连接层，1 表示输出的维度为1，即做的是 n×HIDDEN_SIZE 的矩阵 和 HIDDEN_SIZE×1的矩阵相乘。None指的是不使用激活函数。
+    predictions = tf.contrib.layers.fully_connected(output, 1, None)
+    # 重塑 y 和 predictions 
+    labels = tf.reshape(y, [-1])
+    predictions = tf.reshape(predictions, [-1])
+    # 得到均方损失
+    loss = tf.losses.mean_squared_error(predictions, labels)
+    # 得到训练操作
+    train_op = tf.contrib.layers.optimize_loss(loss, tf.contrib.framework.get_global_step(),
+        optimizer="Adagrad", learning_rate=0.1)
+    return predictions,loss,train_op
 
-    output, _ = tf.nn.static_rnn(cell, x_, dtype = tf.float32)
-    output = output[-1]
 
-    prediction, loss = learn.models.linear_regression(output, y)
-    train_op = tf.contrib.layers.optimize_loss(
-        loss, tf.contrib.framework.get_global_step(), 
-        optimizer="Adagrad", learning_rate = 0.1)
-
-    return prediction, loss, train_op
-
-regressor = learn.SKCompat(learn.Estimator(model_fn=lstm_model))
 test_start = TRAINING_EXAMPLES * SAMPLE_GAP
-print(test_start)
 test_end = (TRAINING_EXAMPLES + TESTING_EXAMPLES) * SAMPLE_GAP
-print(test_end)
+
+print (test_start)
+print (test_end)
 train_X, train_y = generate_data(np.sin(np.linspace(0, test_start, TRAINING_EXAMPLES, dtype = np.float32)))
 test_X , test_y = generate_data(np.sin(np.linspace(test_start, test_end, TESTING_EXAMPLES, dtype = np.float32)))
 
+regressor = learn.SKCompat(learn.Estimator(model_fn=lstm_model))
 regressor.fit(train_X, train_y, batch_size = BATCH_SIZE, steps = TRAINING_STEPS)
 predicted = [[pred] for pred in regressor.predict(test_X)]
 
